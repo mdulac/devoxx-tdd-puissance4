df6ab3d0eda09eff5f34f8c930f192c89a30b063
mdulac <mat.dulac@gmail.com>
2015-03-17 23:55:23 +0100

    Create Puissance 4 implementation class

---
+++ src/main/java/fr/ippon/contest/puissance4/Puissance4Impl.java
@@ -0,0 +1,35 @@
+package fr.ippon.contest.puissance4;
+
+public class Puissance4Impl implements Puissance4 {
+
+
+    @Override
+    public void nouveauJeu() {
+
+    }
+
+    @Override
+    public void chargerJeu(char[][] grille, char tour) {
+
+    }
+
+    @Override
+    public EtatJeu getEtatJeu() {
+        return null;
+    }
+
+    @Override
+    public char getTour() {
+        return 0;
+    }
+
+    @Override
+    public char getOccupant(int ligne, int colonne) {
+        return 0;
+    }
+
+    @Override
+    public void jouer(int colonne) {
+
+    }
+}
