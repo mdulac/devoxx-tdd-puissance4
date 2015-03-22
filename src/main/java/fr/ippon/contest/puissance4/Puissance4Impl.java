af8271c197f6a10582af661a5833abea5de1e256
mdulac <mat.dulac@gmail.com>
2015-03-22 22:54:30 +0100

    Check lines at game loading

--- src/main/java/fr/ippon/contest/puissance4/Puissance4Impl.java
+++ src/main/java/fr/ippon/contest/puissance4/Puissance4Impl.java
@@ -9,6 +9,7 @@ public class Puissance4Impl implements Puissance4 {
 
     private static final int LINES_COUNT = 6;
     private static final int COLUMNS_COUNT = 7;
+
     private static final char EMPTY = '-';
 
     private EtatJeu gameState = EN_COURS;
@@ -22,10 +23,19 @@ public class Puissance4Impl implements Puissance4 {
             , {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}
     };
 
-    private static final char[] players = new char[]{'J', 'R'};
-
-    private int currentPlayerIndex = 0;
     private int empties = LINES_COUNT * COLUMNS_COUNT;
+
+    private static final char FIRST_PLAYER = 'J';
+    private static final char SECOND_PLAYER = 'R';
+    private static final char[] PLAYERS = new char[]{FIRST_PLAYER, SECOND_PLAYER};
+    private int currentPlayerIndex = 0;
+
+    private static final String PLAYER_NOT_VALID = "Player must be valid";
+    private static final String COLUMN_NOT_VALID = "Column must be valid";
+    private static final String GRID_LINES_COUNT_NOT_VALID = "Must have 6 lines";
+    private static final String GRID_COLUMN_COUNT_NOT_VALID = "Must have 7 columns by row";
+    private static final String COLUMN_FULL = "Column is full";
+    private static final String UNKNOWN_PLAYER = "Player doesn't exist";
 
     @Override
     public void nouveauJeu() {
@@ -48,18 +58,29 @@ public class Puissance4Impl implements Puissance4 {
         assertGridIsValid(grille);
 
         if (!isPlayerValid(tour)) {
-            throw new IllegalArgumentException("Player must be valid");
+            throw new IllegalArgumentException(PLAYER_NOT_VALID);
         }
 
         gameGrid = grille;
         currentPlayerIndex = playerIndex(tour);
-        empties = countEmpties();
+        gameState = EN_COURS;
 
-        if (empties <= 0) {
-            gameState = MATCH_NUL;
-        } else {
-            gameState = EN_COURS;
+        for (int l = 0; l < gameGrid.length; l++) {
+            for (int c = 0; c < gameGrid[l].length; c++) {
+                if (gameGrid[l][c] != EMPTY) {
+                    checkLinesForPlayerFrom(getOccupant(l, c), l, c);
+                }
+            }
         }
+
+        if (gameState == EN_COURS) {
+            empties = countEmpties();
+
+            if (empties <= 0) {
+                gameState = MATCH_NUL;
+            }
+        }
+
     }
 
     @Override
@@ -80,7 +101,7 @@ public class Puissance4Impl implements Puissance4 {
     @Override
     public void jouer(int colonne) {
         if (colonne < 0 || colonne > gameGrid.length) {
-            throw new IllegalArgumentException("Column must be valid");
+            throw new IllegalArgumentException(COLUMN_NOT_VALID);
         }
 
         int line = selectFirstLineEmptyAtColumn(colonne);
@@ -92,7 +113,7 @@ public class Puissance4Impl implements Puissance4 {
             gameState = MATCH_NUL;
         }
 
-        checkLinesForCurrentPlayerFrom(line, colonne);
+        checkLinesForPlayerFrom(currentPlayer(), line, colonne);
 
         changePlayer();
     }
@@ -100,29 +121,29 @@ public class Puissance4Impl implements Puissance4 {
     private void assertGridIsValid(char[][] grille) {
 
         if (grille.length != LINES_COUNT) {
-            throw new IllegalArgumentException("Must have 6 lines");
+            throw new IllegalArgumentException(GRID_LINES_COUNT_NOT_VALID);
         }
         for (char[] line : grille) {
             if (line.length != COLUMNS_COUNT) {
-                throw new IllegalArgumentException("Must have 7 columns by row");
+                throw new IllegalArgumentException(GRID_COLUMN_COUNT_NOT_VALID);
             }
         }
     }
 
     private int playerIndex(char player) {
-        for (int i = 0; i <= players.length - 1; i++) {
-            if (player == players[i]) {
+        for (int i = 0; i <= PLAYERS.length - 1; i++) {
+            if (player == PLAYERS[i]) {
                 return i;
             }
         }
 
-        throw new IllegalStateException("Player doesn't exist");
+        throw new IllegalStateException(UNKNOWN_PLAYER);
     }
 
     private boolean isPlayerValid(char player) {
         try {
             playerIndex(player);
-        } catch (Exception e) {
+        } catch (IllegalStateException e) {
             return false;
         }
 
@@ -136,11 +157,11 @@ public class Puissance4Impl implements Puissance4 {
             }
         }
 
-        throw new IllegalStateException("Column is full");
+        throw new IllegalStateException(COLUMN_FULL);
     }
 
     private char currentPlayer() {
-        return players[currentPlayerIndex];
+        return PLAYERS[currentPlayerIndex];
     }
 
     private void changePlayer() {
@@ -160,92 +181,20 @@ public class Puissance4Impl implements Puissance4 {
         return empties;
     }
 
-    private void checkLinesForCurrentPlayerFrom(int line, int column) {
+    private void checkLinesForPlayerFrom(char player, int line, int column) {
+        checkVerticalLine(player, line, column);
+        checkHorizontalLine(player, line, column);
+        checkFirstDiagonalLine(player, line, column);
+        checkSecondDiagonalLine(player, line, column);
+    }
 
-        // Line length is at least equals to the player position
+    private void checkSecondDiagonalLine(char player, int line, int column) {
         int lineLength = 1;
-        int currentPlayer = currentPlayer();
 
-        // Check vertical line
-        for (int i = line - 1; i >= 0; i--) {
-            if (gameGrid[i][column] == currentPlayer) {
-                lineLength++;
-            } else {
-                break;
-            }
-        }
-
-        for (int i = line + 1; i < gameGrid.length; i++) {
-            if (gameGrid[i][column] == currentPlayer) {
-                lineLength++;
-            } else {
-                break;
-            }
-        }
-
-        if (lineLength >= 4) {
-            gameState = currentPlayer == 'J' ? JAUNE_GAGNE : ROUGE_GAGNE;
-        }
-
-        lineLength = 1;
-
-        // Check horizontal line
-        for (int i = column - 1; i >= 0; i--) {
-            if (gameGrid[line][i] == currentPlayer) {
-                lineLength++;
-            } else {
-                break;
-            }
-        }
-
-        for (int i = column + 1; i < gameGrid[0].length; i++) {
-            if (gameGrid[line][i] == currentPlayer) {
-                lineLength++;
-            } else {
-                break;
-            }
-        }
-
-        if (lineLength >= 4) {
-            gameState = currentPlayer == 'J' ? JAUNE_GAGNE : ROUGE_GAGNE;
-        }
-
-        lineLength = 1;
-
-        // Check \ diagonal line
-        int j = line - 1;
+        int j = line + 1;
 
         for (int i = column - 1; i >= 0; i--) {
-            if (j >= 0 && gameGrid[j][i] == currentPlayer) {
-                lineLength++;
-                j--;
-            } else {
-                break;
-            }
-        }
-
-        j = line + 1;
-
-        for (int i = column + 1; i < gameGrid[0].length; i++) {
-            if (j < gameGrid.length && gameGrid[j][i] == currentPlayer) {
-                lineLength++;
-                j++;
-            } else {
-                break;
-            }
-        }
-
-        if (lineLength >= 4) {
-            gameState = currentPlayer == 'J' ? JAUNE_GAGNE : ROUGE_GAGNE;
-        }
-
-        lineLength = 1;
-
-        // Check / diagonal line
-        j = line + 1;
-
-        for (int i = column - 1; i >= 0; i--) {
-            if (j < gameGrid.length && gameGrid[j][i] == currentPlayer) {
+            if (j < gameGrid.length && gameGrid[j][i] == player) {
                 lineLength++;
                 j++;
             } else {
@@ -256,7 +205,7 @@ public class Puissance4Impl implements Puissance4 {
         j = line - 1;
 
         for (int i = column + 1; i < gameGrid[0].length; i++) {
-            if (j >= 0 && gameGrid[j][i] == currentPlayer) {
+            if (j >= 0 && gameGrid[j][i] == player) {
                 lineLength++;
                 j--;
             } else {
@@ -264,10 +213,85 @@ public class Puissance4Impl implements Puissance4 {
             }
         }
 
-        if (lineLength >= 4) {
-            gameState = currentPlayer == 'J' ? JAUNE_GAGNE : ROUGE_GAGNE;
+        hasPlayerWon(player, lineLength);
+    }
+
+    private void checkFirstDiagonalLine(char player, int line, int column) {
+        int lineLength = 1;
+
+        int j = line - 1;
+
+        for (int i = column - 1; i >= 0; i--) {
+            if (j >= 0 && gameGrid[j][i] == player) {
+                lineLength++;
+                j--;
+            } else {
+                break;
+            }
         }
 
+        j = line + 1;
+
+        for (int i = column + 1; i < gameGrid[0].length; i++) {
+            if (j < gameGrid.length && gameGrid[j][i] == player) {
+                lineLength++;
+                j++;
+            } else {
+                break;
+            }
+        }
+
+        hasPlayerWon(player, lineLength);
+    }
+
+    private void checkHorizontalLine(char player, int line, int column) {
+        int lineLength = 1;
+
+        for (int i = column - 1; i >= 0; i--) {
+            if (gameGrid[line][i] == player) {
+                lineLength++;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = column + 1; i < gameGrid[0].length; i++) {
+            if (gameGrid[line][i] == player) {
+                lineLength++;
+            } else {
+                break;
+            }
+        }
+
+        hasPlayerWon(player, lineLength);
+    }
+
+    private void checkVerticalLine(char player, int line, int column) {
+        int lineLength = 1;
+
+        for (int i = line - 1; i >= 0; i--) {
+            if (gameGrid[i][column] == player) {
+                lineLength++;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = line + 1; i < gameGrid.length; i++) {
+            if (gameGrid[i][column] == player) {
+                lineLength++;
+            } else {
+                break;
+            }
+        }
+
+        hasPlayerWon(player, lineLength);
+    }
+
+    private void hasPlayerWon(char player, int lineLength) {
+        if (lineLength >= 4) {
+            gameState = player == FIRST_PLAYER ? JAUNE_GAGNE : ROUGE_GAGNE;
+        }
     }
 
 }
